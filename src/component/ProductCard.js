7fd6bbfd237d93f6aa36f13be37240bf481d699b
Hyeri Yu <yhr8340@naver.com>
2024-03-28 01:26:32 +0900

    first commit

---
+++ src/component/ProductCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+      <img src='https://lp2.hm.com/hmgoepprod?set=source[/76/18/76180f21209f47951e2de2c9e1a02cc239df8251.jpg],origin[dam],category[],type[LOOKBOOK],res[z],hmver[1]&call=url[file:/product/main]' alt='옷' />
+      <div>Conscious choice</div>
+      <div>벨티드 트윌 코트</div>
+      <div>99990원</div>
+      <div>신제품</div>
+    </div>
+  )
+}
+
+export default ProductCard
